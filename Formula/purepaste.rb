fed487e8b682196c12e00d69b99949a5524190d0
Andrew Selkirk <alselkirk@gmail.com>
2022-04-09 15:05:11 -0400

    update

--- Formula/purepaste.rb
+++ Formula/purepaste.rb
@@ -2,7 +2,7 @@ class Purepaste < Formula
 
 	desc "Pure Paste"
 	homepage "https://apps.apple.com/us/app/pure-paste/id1611378436?mt=12"
-	url ""
+	url "https://wordpress.org/latest.tar.gz"
 	version "1.3.2"
 
 
