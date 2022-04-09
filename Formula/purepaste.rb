0d0abd7afa730a3e6052334489b2dc9fef88e2ab
Andrew Selkirk <alselkirk@gmail.com>
2022-04-09 15:02:16 -0400

    update

--- Formula/purepaste.rb
+++ Formula/purepaste.rb
@@ -2,12 +2,12 @@ class PurePaste < Formula
 
 	desc "Pure Paste"
 	homepage "https://apps.apple.com/us/app/pure-paste/id1611378436?mt=12"
+	url ""
 	version "1.3.2"
 
-	depends_on mas
 
 	def install
-		system "/usr/local/bin/mas", "install", "1611378436"
+		system "python", "install", "1611378436"
 	end
 
 end
