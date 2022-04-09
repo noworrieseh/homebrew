f48ac043e563a33a8dc346c8ee2cfa5268058b62
Andrew Selkirk <alselkirk@gmail.com>
2022-04-09 15:14:01 -0400

    update

--- Formula/purepaste.rb
+++ Formula/purepaste.rb
@@ -2,12 +2,16 @@ class Purepaste < Formula
 
 	desc "Pure Paste"
 	homepage "https://apps.apple.com/us/app/pure-paste/id1611378436?mt=12"
-	url "https://wordpress.org/latest.tar.gz"
+	url "file:///dev/null"
 	version "1.3.2"
 
 
 	def install
 		system "/usr/local/bin/mas", "install", "1611378436"
+	end
+
+	test do
+		assert_predicate "/Applications/Pure\ Paste.app", :exist?
 	end
 
 end
