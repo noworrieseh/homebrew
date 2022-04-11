33bf4d515efe4bf4e9e8f50b156996bdcaeee22c
Andrew Selkirk <alselkirk@gmail.com>
2022-04-10 22:45:46 -0400

    update

--- Formula/garageband.rb
+++ Formula/garageband.rb
@@ -4,6 +4,7 @@ class Garageband < Formula
 	desc "GarageBand"
 	homepage "https://apps.apple.com/us/app/id@@masid"
 	url "https://raw.githubusercontent.com/noworrieseh/homebrew/main/bin/buildapp.sh"
+	sha256 "27a6d633a69731db0af61573558c33b87c5b388dbc0d2d3696c428dc10619305"
 	version "1.0"
 
 
