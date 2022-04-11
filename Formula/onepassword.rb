309a74670a802938a2fd82da8c6cc77148cd4e28
Andrew Selkirk <alselkirk@gmail.com>
2022-04-10 22:49:16 -0400

    update

--- Formula/onepassword.rb
+++ Formula/onepassword.rb
@@ -1,7 +1,7 @@
 class Onepassword < Formula
 	@@masid = 1333542190
 
-	desc "1Password"
+	desc "1Password 7"
 	homepage "https://apps.apple.com/us/app/id@@masid"
 	url "https://raw.githubusercontent.com/noworrieseh/homebrew/main/bin/buildapp.sh"
 	sha256 "27a6d633a69731db0af61573558c33b87c5b388dbc0d2d3696c428dc10619305"
@@ -16,7 +16,7 @@ class Onepassword < Formula
 	end
 
 	test do
-		assert_predicate "/Applications/1Password.app", :exist?
+		assert_predicate "/Applications/1Password 7.app", :exist?
 	end
 
 end
